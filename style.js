69005df3c8a97fe3aeee1fe51931b0c88abe4e97
SonjaElena <sonja.koppenhoefer@web.de>
2024-02-29 13:43:08 +0100

    new codes

--- style.js
+++ style.js
@@ -404,4 +404,37 @@ function killer(suspectInfo, dead) {
   return murderer.join(", ");
 }
 
-// 6 kyu - Longest palindrome
+// extract-class-name - recap-day
+function extractClassName(sessionTitle) {
+  sessionTitle = sessionTitle.split(" ");
+  let year = sessionTitle[1];
+  let monthletter = sessionTitle[2];
+
+  const monthMap = {
+    Januar: "01",
+    Februar: "02",
+    März: "03",
+    Maerz: "03",
+    April: "04",
+    Mai: "05",
+    Juni: "06",
+    Juli: "07",
+    August: "08",
+    September: "09",
+    Oktober: "10",
+    November: "11",
+    Dezember: "12",
+  };
+
+  let month = monthMap[monthletter];
+
+  if (
+    sessionTitle[0] === "Class" &&
+    /^\d{4}$/.test(year) &&
+    monthletter in monthMap
+  ) {
+    return "'" + year + "-" + month + "'";
+  } else {
+    return null;
+  }
+}
